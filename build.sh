92f83adc2201073d2674c1f0e2f76f73dc603906
Sheep Sun <sunxiaoyang2003@gmail.com>
2021-07-22 18:18:39 +0800

    Add ELF build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 hexagon-gcc memory_server.c -o memory_server.o -fpic -nostdlib
 hexagon-objcopy -O binary memory_server.o memory_server.bin
+hexagon-ld -T memory_server.ld memory_server.o -o memory_server.elf
